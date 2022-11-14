6dab71beefafb80e66597f05a151bf89ca580d64
Preston Swigart <prestonlswigart@gmail.com>
2022-11-14 10:49:45 -0500

    11/14 10:49am

---
+++ src/BMICalculator.java
@@ -0,0 +1,13 @@
+public class BMICalculator {
+    /** Convert English to metric units, perform the BMI calculation.
+     * NOTE: this method must properly handle bad data */
+    public static double computeBMI(int inches, int pounds) {
+        return 0.0;
+    }
+    /** Uses a Scanner to prompt the user for info, process the
+     * feet/inches conversion, calls the computeBMI method and prints the
+     * correct information. */
+    public static void main(String[] args){
+
+    }
+}
