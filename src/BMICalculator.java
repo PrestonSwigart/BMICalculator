654c232e0090d27e2a463b993b92dfa83404a1d4
Preston Swigart <prestonlswigart@gmail.com>
2022-11-16 08:45:53 -0500

    11/16 8:45am

--- src/BMICalculator.java
+++ src/BMICalculator.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class BMICalculator {
@@ -7,7 +8,10 @@ public class BMICalculator {
         double kgs, meters;
         meters = inches * 0.0254;
         kgs = pounds * 0.454;
-        return kgs / Math.pow(meters, 2);
+        if(inches <= 0 || pounds <= 0)
+            return 0;
+        else
+            return kgs / Math.pow(meters, 2);
     }
 
     /** Uses a Scanner to prompt the user for info, process the
@@ -17,6 +21,7 @@ public class BMICalculator {
         Scanner in = new Scanner(System.in);
         String userin = new String();
         int inches = 0, pounds = 0;
+        DecimalFormat df = new DecimalFormat("0.00");
         while(true){
             System.out.print("Enter your height in feet and inches (Ex 6'1\") or 0 to quit:");
             userin = in.nextLine();
@@ -27,7 +32,7 @@ public class BMICalculator {
                 inches += Integer.parseInt(userin.substring(userin.indexOf("'")+1, userin.indexOf("\"")));
                 System.out.println("Enter your weight in pounds: ");
                 pounds = Integer.parseInt(in.nextLine());
-
+                System.out.println("Your BMI, expressed as weight(kg)/height(m)^2: " + df.format(computeBMI(inches, pounds)) + " kg/m^2");
             }
             catch(Exception e){
                 System.out.println("There was an error, see here " + e.toString());
